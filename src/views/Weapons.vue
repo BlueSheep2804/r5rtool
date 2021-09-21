60562dbc911deb90ff29bbcd4fb7b2e3de4f515a
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2021-09-21 15:34:03 +0900

    [ADD]基本的なテキストフィールドを追加

--- src/views/Weapons.vue
+++ src/views/Weapons.vue
@@ -1,6 +1,28 @@
 <template>
   <v-container>
     <h1>Weapons</h1>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-text-field
+          label="武器名"
+          placeholder="例: R-99"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-text-field
+          label="弾薬数"
+          type="number"
+        ></v-text-field>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
