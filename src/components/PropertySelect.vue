c6575dc542515ab1625a10f2b39c2a5b8c6408e1
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2021-09-25 10:54:50 +0900

    [ADD]selectのcomponentを追加

---
+++ src/components/PropertySelect.vue
@@ -0,0 +1,33 @@
+<template>
+<v-row>
+  <v-col
+    cols="12"
+    sm="4"
+  >
+    <v-select
+      :label="label"
+      :items="items"
+      v-model="weaponProperty"
+    ></v-select>
+  </v-col>
+</v-row>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PropertySelect extends Vue {
+  @Prop() private label!: string;
+  @Prop() private items!: string;
+  @Prop() private weaponKey!: string;
+
+  private get weaponProperty(): string {
+    return this.$store.state.weapon[this.weaponKey]
+  }
+  private set weaponProperty(value: string) {
+    const args: string[] = [this.weaponKey, value]
+    this.$store.commit('weaponPropertyUpdate', args)
+  }
+}
+</script>
