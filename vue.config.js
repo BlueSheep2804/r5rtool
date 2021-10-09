36eb39b8a9a30b7e428620e16ec46d3210f21506
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2021-10-09 16:08:54 +0900

    [ADD]vue-i18nを導入

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,18 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+
   configureWebpack: {
     devtool: 'source-map'
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'ja',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableBridge: false
+    }
   }
 }
